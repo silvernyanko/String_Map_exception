a8afabd3a10160e2a842e1c2d27189e29dd6170b
silvernyanko <145736051+silvernyanko@users.noreply.github.com>
2023-09-29 17:10:19 +0900

    Add files via upload

---
+++ assignment3/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> northEuropeMap = new HashMap<>();
+        northEuropeMap.put("フィンランド", "ヘルシンキ");
+        northEuropeMap.put("ノルウェー", "オスロ");
+        northEuropeMap.put("スウェーデン", "ストックホルム");
+        northEuropeMap.put("デンマーク", "コペンハーゲン");
+
+        try {
+            for (String key : northEuropeMap.keySet()) {
+                if (key.equals("デンマーク")) {
+                    throw new Exception();
+                } else {
+                    System.out.println(key + "の首都は" + northEuropeMap.get(key));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("エラー: インデックスが範囲外です");
+        }
+    }
+}
+
+
+
+//課題のソースコードをGitHubにpushしてレビューを依頼する
